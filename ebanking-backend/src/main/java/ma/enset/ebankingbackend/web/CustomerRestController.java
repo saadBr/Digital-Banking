cca1c69c6779579dad540ffb16920849a3b18574
saade <saadelmabrouk18@gmail.com>
2025-07-07 19:58:12 +0100

    fix: revert merge conflict and restore clean backend structure

--- ebanking-backend/src/main/java/ma/enset/ebankingbackend/web/CustomerRestController.java
+++ ebanking-backend/src/main/java/ma/enset/ebankingbackend/web/CustomerRestController.java
@@ -6,6 +6,7 @@ import ma.enset.ebankingbackend.dtos.CustomerDTO;
 import ma.enset.ebankingbackend.entities.Customer;
 import ma.enset.ebankingbackend.exceptions.CustomerNotFoundException;
 import ma.enset.ebankingbackend.services.BankAccountService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,23 +17,28 @@ import java.util.List;
 public class CustomerRestController {
     private BankAccountService bankAccountService;
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public List<CustomerDTO> customers() {
         return bankAccountService.getCustomers();
     }
     @GetMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
         return bankAccountService.getCustomerById(customerId);
     }
     @PostMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public CustomerDTO createCustomer(@RequestBody CustomerDTO customerDTO) {
         return bankAccountService.saveCustomer(customerDTO);
     }
     @PutMapping("/customers/{customerID}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public CustomerDTO updateCustomer(@PathVariable long customerID, @RequestBody CustomerDTO customerDTO) throws CustomerNotFoundException {
         customerDTO.setId(customerID);
         return bankAccountService.updateCustomer(customerDTO);
     }
     @DeleteMapping("/customers/{customerID}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public void deleteCustomer(@PathVariable long customerID) throws CustomerNotFoundException {
         bankAccountService.deleteCustomer(customerID);
     }

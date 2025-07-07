a856a5be157c06b31003611ecc0cbdda1404a5f1
saade <saadelmabrouk18@gmail.com>
2025-07-07 21:36:11 +0100

    resolve cors configuration link issue

--- ebanking-backend/src/main/java/ma/enset/ebankingbackend/security/SecurityConfig.java
+++ ebanking-backend/src/main/java/ma/enset/ebankingbackend/security/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));

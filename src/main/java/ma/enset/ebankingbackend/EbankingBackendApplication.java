c9e30959dd4995180c445875e9fcec99e0c7b8e8
saade <saadelmabrouk18@gmail.com>
2025-07-21 00:57:59 +0100

    Remove dotenv, use Heroku env vars

--- src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
+++ src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
@@ -35,11 +35,6 @@ import java.util.stream.Stream;
 public class EbankingBackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
-
-        System.setProperty("spring.data.mongodb.uri", dotenv.get("MONGODB_URI"));
-        System.setProperty("spring.data.mongodb.database", dotenv.get("MONGODB_DATABASE"));
-        System.setProperty("jwt.secret", dotenv.get("JWT_SECRET"));
         SpringApplication.run(EbankingBackendApplication.class, args);
     }
 

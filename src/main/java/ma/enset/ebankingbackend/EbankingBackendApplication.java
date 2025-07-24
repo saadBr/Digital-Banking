f0742579f0b005589f055b2f33b25bc670432518
saade <saadelmabrouk18@gmail.com>
2025-07-24 21:17:11 +0100

    add missing functions

--- src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
+++ src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
@@ -1,5 +1,6 @@
 package ma.enset.ebankingbackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EbankingBackendApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().load();
+
+        System.setProperty("spring.data.mongodb.uri", dotenv.get("MONGODB_URI"));
+        System.setProperty("spring.data.mongodb.database", dotenv.get("MONGODB_DATABASE"));
+        System.setProperty("jwt.secret", dotenv.get("JWT_SECRET"));
+
         SpringApplication.run(EbankingBackendApplication.class, args);
     }
 
